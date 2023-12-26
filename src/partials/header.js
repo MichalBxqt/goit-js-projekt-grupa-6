3c8c7bf877505a242b002b30018eb18f71bcdb66
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-12-26 15:54:13 +0100

    change header

--- src/partials/header.js
+++ src/partials/header.js
@@ -34,3 +34,17 @@ login.addEventListener('click', logoutShow);
 function logoutShow() {
   logout.classList.toggle('is-open');
 }
+
+
+function addCurrentClassToMenuLink() {
+    var path = window.location.pathname;
+    var menuLinks = document.querySelectorAll('.menu__link');
+    menuLinks.forEach(function(link) {
+      var linkPath = link.getAttribute('href');
+      if (path === linkPath) {
+        link.classList.add('menu__link--current');
+      }
+    });
+  }
+  
+  addCurrentClassToMenuLink();
