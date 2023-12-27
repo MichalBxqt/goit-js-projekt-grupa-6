bace9ce053854e13d9e2f5ff368d86dcc1da89e9
MichalBxqt <michal.blaszczyk00@gmail.com>
2023-12-27 13:47:42 +0000

    Deploying to gh-pages from @ MichalBxqt/goit-js-projekt-grupa-6@b935b05840d7e4ed50e05faca0b2c908e9a20e20 🚀

---
+++ index.d3158c57.js
@@ -0,0 +1,2 @@
+const d=document.querySelector(".loader");d.classList.add("hidden");
+//# sourceMappingURL=index.d3158c57.js.map
---
+++ index.def9e101.js
@@ -0,0 +1,2 @@
+!function(){var d=document.querySelector(".loader");d.classList.add("hidden")}();
+//# sourceMappingURL=index.def9e101.js.map
