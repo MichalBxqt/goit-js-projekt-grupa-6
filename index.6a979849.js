2ad76114e630c6aff0cf64871a409dbbfebcb1fe
MichalBxqt <michal.blaszczyk00@gmail.com>
2024-01-02 17:46:13 +0000

    Deploying to gh-pages from @ MichalBxqt/goit-js-projekt-grupa-6@ac882e9842063ebcdf55dc3834bcbef9963ce750 🚀

--- index.6a979849.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("myModal"),n=document.getElementById("myBtn"),t=document.querySelector(".popup__close-button-span");n.onclick=function(){e.style.display="block"},t.onclick=function(){e.style.display="none"},window.onclick=function(n){n.target==e&&(e.style.display="none")},document.addEventListener("keydown",n=>{n.preventDefault(),"Escape"===n.key&&(e.style.display="none")});
-//# sourceMappingURL=index.6a979849.js.map
